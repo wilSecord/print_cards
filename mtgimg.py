3a8f8fe0a5ab788f85700deee9483d9ea3329794
wilSecord <wilsonsecord@gmail.com>
2023-12-16 15:00:13 -0500

    optimizations

--- mtgimg.py
+++ mtgimg.py
@@ -3,17 +3,10 @@ import time
 import requests
 import cv2
 import numpy as np
+import os
 
-def make_image(cn, full):
-    time.sleep(0.1)
-    card = scrython.cards.Named(fuzzy=cn)
-    file = cn + '.jpg'
-    req = requests.get(card.image_uris(0, 'normal'))
-
-    with open(file, 'wb') as f:
-        f.write(req.content)
-
-    img = cv2.imread(file)
+def make_image(fn, full):
+    img = cv2.imread(fn)
     img = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (3, 3), 0)
     t1, t2 = 10, 360
     can = cv2.Canny(image=img, threshold1=t1, threshold2=t2)
@@ -27,4 +20,13 @@ def make_image(cn, full):
         can = cv2.resize(can, (300, 512))
     can = cv2.rotate(can, cv2.ROTATE_90_COUNTERCLOCKWISE)
     
-    cv2.imwrite(file, cv2.bitwise_not(can))
+    cv2.imwrite(fn, cv2.bitwise_not(can))
+
+def collect(cn, path):
+    time.sleep(0.1)
+    card = scrython.cards.Named(fuzzy=cn)
+    file = cn + '.jpg'
+    req = requests.get(card.image_uris(0, 'normal'))
+    
+    with open(path + file, 'wb') as f:
+        f.write(req.content)
--- print_card.py
+++ print_card.py
@@ -1,13 +1,8 @@
-import sys
-import re
-import scrython
+import sys, os, re, scrython, six, time
 from escpos import printer
 from escpos.constants import GS, ESC
-import six
-import time
-from mtgimg import make_image
+from mtgimg import make_image, collect
 import xml.etree.ElementTree as et
-import os
 
 opt = sys.argv[1]
 
@@ -22,6 +17,7 @@ e = printer.Usb(0x04b8, 0x0202)
 b = []
 mb = []
 sb = []
+
 if len(sys.argv) > 4:
     full = sys.argv[4].upper() == "TRUE"
 
@@ -29,51 +25,58 @@ match opt:
     case "d":
         deck_loc = sys.argv[2]
         sb_bool = sys.argv[3]
-        with open(deck_loc) as f:
-            root = et.parse(f).getroot()
-            for item in root:
-                if item.keys():
-                    name = item.attrib["Name"]
-                    amnt = int(item.attrib["Quantity"])
-                    if item.attrib["Sideboard"] == "false":
-                        for i in range(amnt):
-                            if name not in basics:
-                                mb.append(scrython.cards.Named(fuzzy=name))
-                            else:
-                                if (amnt, name) not in b:
-                                    b.append((amnt, name))
-                    elif sb_bool == "y":
-                        sb.append(scrython.cards.Named(fuzzy=name))
-        
+        f = open(deck_loc)
+        root = et.parse(f).getroot()
+        for item in root:
+            if item.keys() and item.attrib["Sideboard"] == "false":
+                name = item.attrib["Name"]
+                amnt = int(item.attrib["Quantity"])
+                for i in range(amnt):
+                    if name not in basics:
+                        mb.append((amnt, name))
+                    elif (amnt, name) not in b:
+                        b.append((amnt, name))
+            elif item.keys() and sb_bool == "y":
+                sb.append((amnt, name))
+        f.close()
         for item in mb:
-            time.sleep(0.1)
-            make_image(item.name(), full)
-            e.image("buf.jpg")
-            if not full:
-                e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
+            collect(item[1], "cards/mb/")
+
+        for item in sb:
+            collect(item[1], "cards/sb/")
+
+        for item in os.listdir("cards/mb/"):
+            make_image("cards/mb/" + item, full)
+            for i in range(amnt):
+                e.image("cards/mb/" + item)
+                if not full:
+                    e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
+            os.remove("cards/mb/" + item)
         
         for item in b:
-            time.sleep(0.1)
             e.text(str(item[0]) + ' ' + item[1] + '\n')
         if len(b):
             e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
         
         e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
-
-        for item in sb:
-            time.sleep(0.1)
-            make_image(item.name(), full)
-            e.image("buf.jpg")
-            if not full:
-                e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
+        
+        for item in os.listdir("cards/sb/"):
+            make_image("cards/sb/" + item, full)
+            for i in range(amnt):
+                e.image("cards/sb/" + item)
+            os.remove("cards/sb/" + item)
+        e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
 
     case "c":
         card = sys.argv[2]
+        fn = card + ".jpg"
         count = int(sys.argv[3])
         time.sleep(0.1)
         for i in range(count):
-            make_image(card, full)
-            e.image("buf.jpg")
+            collect(card, "")
+            make_image(fn, full)
+            e.image(fn)
             e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
+        os.remove(fn)
     case "r":
         e._raw(GS + b'V' + six.int2byte(66) + b'\x00')
